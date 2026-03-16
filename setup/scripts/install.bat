01368b5ceb92663cba836a41f3a6c41a94972e49
KingIwrs <53910323+KingIwrs@users.noreply.github.com>
2026-03-16 17:46:23 +0100

    I honestly don't know why it is here.

--- setup/scripts/install.bat
+++ setup/scripts/install.bat
@@ -4,13 +4,19 @@ winget install -e --id LGUG2Z.komorebi
 winget install -e --id LGUG2Z.whkd
 winget install -e --id LGUG2Z.masir
 winget install -e --id AmN.yasb
+
 winget install -e --id Nushell.Nushell
 winget install -e --id wez.wezterm
+
 winget install -e --id JesseDuffield.lazygit
 winget install -e --id GitHub.cli
+
 winget install -e --id Neovim.Neovim
+
 winget install -e --id AutoHotkey.AutoHotkey
+
 winget install -e --id Flow-Launcher.Flow-Launcher
+
 winget install -e --id sharkdp.bat
 
 winget install -e --id sxyazi.yazi
@@ -31,7 +37,6 @@ winget install -e --id GNU.Wget2
 winget install -e --id Flameshot.Flameshot
 
 pip install pymatrix-rain
-pip install windows-curses
 winget install -e --id gsass1.NTop
 winget install -e --id Clement.bottom
 
